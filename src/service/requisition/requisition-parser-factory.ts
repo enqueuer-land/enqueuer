e81e3c232dba84b2a05a8570595d902e3051e18b
Guilherme Moraes <gmoraes@thoughtworks.com>
2018-02-20 15:15:50 -0300

    Variables are being replaced. Mote to self: stop reinventing the wheel

--- src/service/requisition/requisition-parser-factory.ts
+++ src/service/requisition/requisition-parser-factory.ts
@@ -1,38 +1,26 @@
 import { MqttRequisitionParser } from "../../mqtt/mqtt-requisition-parser";
 import { MessengerService } from "../messenger-service";
 import { MqttService } from "../mqtt-service";
+const jsonSub = require('json-sub')();
+
 
 export class RequisitionParserFactory {
     createService(requisitionMessage: string): MessengerService {
-        const requisition = JSON.parse(requisitionMessage);
-        const variablesReplacedRequisition = this.replaceVariables(requisition);
-        if (requisition.protocol == "mqtt")
+        const parsedRequisition = JSON.parse(requisitionMessage);
+        const variablesReplacedRequisition = this.replaceVariables(parsedRequisition);
+        if (parsedRequisition.protocol == "mqtt")
             return new MqttService(new MqttRequisitionParser().parse(variablesReplacedRequisition));
-        throw new Error(`Undefined requisition protocol: ${requisition.protocol}`);
+        throw new Error(`Undefined requisition protocol: ${parsedRequisition.protocol}`);
     }
     
-    private replaceVariables(requisition: any): string {
-        var clone = Object.assign({}, requisition);
-        delete clone.variables;
-        let stringifiedRequisition = JSON.stringify(clone);
+    private replaceVariables(parsedRequisition: any): string {
+        var requisitionWithNoVariables = Object.assign({}, parsedRequisition);
+        let variables = parsedRequisition.variables;
+        
+        delete requisitionWithNoVariables.variables;
+    
+        var add = jsonSub.addresser(requisitionWithNoVariables, variables);
 
-        for (const variable in requisition.variables) {
-            console.log(`Replacing {{${variable}}} for ${requisition.variables[variable]}`)
-            const varValue = requisition.variables[variable];
-                
-            stringifiedRequisition = RequisitionParserFactory.replaceAll(stringifiedRequisition,
-                                            "{{"+variable+"}}",
-                                            "'"+requisition.variables[variable].toString()+"'");
-        }
-        console.log(JSON.parse(stringifiedRequisition));
-        return stringifiedRequisition;
-    }
-
-    private static escapeRegExp(str: string): string {
-        return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
-    }
-
-    private static replaceAll(str: string, find: string, replace: string): string {
-        return str.replace(new RegExp(this.escapeRegExp(find), 'g'), replace);
+        return JSON.stringify(add);
     }
 }
