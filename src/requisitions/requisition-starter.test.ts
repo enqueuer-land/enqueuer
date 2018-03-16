6c3a9cc19e68451de033367057228ff82d31ef2a
Guilherme Moraes <gmoraes@thoughtworks.com>
2018-03-16 17:52:16 -0500

    Creating requisitionStarter test

---
+++ src/requisitions/requisition-starter.test.ts
@@ -0,0 +1,60 @@
+import {RequisitionStarter} from "./requisition-starter";
+import {MultiPublisher} from "../publishers/multi-publisher";
+import {RequisitionRunner} from "./requisition-runner";
+import {RequisitionModel} from "./models/requisition-model";
+
+
+const publishMock = jest.fn();
+jest.mock("../publishers/multi-publisher");
+MultiPublisher.mockImplementation(() => {
+    return {
+        publish: publishMock
+    };
+});
+
+const startMock = jest.fn((onFinish: Function) => {
+    return onFinish("report");
+});
+jest.mock("./requisition-runner");
+RequisitionRunner.mockImplementation(() => {
+    return {
+        start: startMock
+    };
+});
+
+describe("RequisitionStarter",() => {
+    let createModel = function () {
+        return {
+            id: "anyStuff",
+            requisitionVersion: "any",
+            subscriptions: [],
+            startEvent: {},
+            reports: [{type: "ahoy"}]
+        };
+    };
+
+    it("Should initialize properly", () => {
+        const model: RequisitionModel = createModel();
+
+        
+        new RequisitionStarter(model);
+
+
+        expect(MultiPublisher).toHaveBeenCalledTimes(1);
+        expect(MultiPublisher).toHaveBeenCalledWith(model.reports);
+        expect(RequisitionRunner).toHaveBeenCalledTimes(1);
+        expect(RequisitionRunner).toHaveBeenCalledWith(model);
+    })
+
+    it("Should start requisitionRunner", () => {
+        const model: RequisitionModel = createModel();
+        const requisitionStarter: RequisitionStarter = new RequisitionStarter(model);
+
+
+        requisitionStarter.start();
+
+
+        expect(startMock).toHaveBeenCalledTimes(1);
+        expect(publishMock).toHaveBeenCalledWith("report");
+    })
+})
