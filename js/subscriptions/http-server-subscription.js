d871941cd51775d4d2b7081566ed06306ea85d81
Guilherme Moraes <gmoraes@thoughtworks.com>
2018-07-18 12:57:21 -0300

    Enabling to subscribe in multiple http endpoints

--- js/subscriptions/http-server-subscription.js
+++ js/subscriptions/http-server-subscription.js
@@ -17,22 +17,11 @@ const logger_1 = require("../loggers/logger");
 const conditional_injector_1 = require("conditional-injector");
 const util_1 = require("util");
 const express_1 = __importDefault(require("express"));
-let HttpServerSubscription = class HttpServerSubscription extends subscription_1.Subscription {
+let HttpServerSubscription = HttpServerSubscription_1 = class HttpServerSubscription extends subscription_1.Subscription {
     constructor(subscriptionAttributes) {
         super(subscriptionAttributes);
         this.response = {};
-        this.app = express_1.default();
-        this.app.use((req, res, next) => {
-            req.setEncoding('utf8');
-            req.rawBody = '';
-            req.on('data', function (chunk) {
-                req.rawBody += chunk;
-            });
-            req.on('end', function () {
-                logger_1.Logger.trace(`Http subscription read ${req.rawBody}`);
-                next();
-            });
-        });
+        this.initializeExpressApp();
         this.port = subscriptionAttributes.port;
         this.endpoint = subscriptionAttributes.endpoint;
         this.method = subscriptionAttributes.method;
@@ -40,8 +29,8 @@ let HttpServerSubscription = class HttpServerSubscription extends subscription_1
         this.response.status = this.response.status || 200;
     }
     receiveMessage() {
-        return new Promise((resolve, reject) => {
-            this.app.all(this.endpoint, (request, response) => {
+        return new Promise((resolve) => {
+            HttpServerSubscription_1.app.all(this.endpoint, (request, response) => {
                 const payload = request.rawBody;
                 if (util_1.isNullOrUndefined(this.response.payload)) {
                     this.response.payload = payload;
@@ -61,23 +50,41 @@ let HttpServerSubscription = class HttpServerSubscription extends subscription_1
     }
     connect() {
         return new Promise((resolve, reject) => {
-            this.server = this.app.listen(this.port, (err) => {
-                if (err) {
-                    reject(err);
-                }
+            if (HttpServerSubscription_1.server) {
                 resolve();
-            });
+            }
+            else {
+                HttpServerSubscription_1.server = HttpServerSubscription_1.app.listen(this.port, (err) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve();
+                });
+            }
         });
     }
-    unsubscribe() {
-        if (this.server) {
-            this.server.close();
+    initializeExpressApp() {
+        if (!HttpServerSubscription_1.app) {
+            HttpServerSubscription_1.app = express_1.default();
+            HttpServerSubscription_1.app.use((req, res, next) => {
+                req.setEncoding('utf8');
+                req.rawBody = '';
+                req.on('data', function (chunk) {
+                    req.rawBody += chunk;
+                });
+                req.on('end', function () {
+                    logger_1.Logger.trace(`Http subscription read ${req.rawBody}`);
+                    next();
+                });
+            });
         }
-        delete this.server;
     }
 };
-HttpServerSubscription = __decorate([
+HttpServerSubscription.app = null;
+HttpServerSubscription.server = null;
+HttpServerSubscription = HttpServerSubscription_1 = __decorate([
     conditional_injector_1.Injectable({ predicate: (subscriptionAttributes) => subscriptionAttributes.type === 'http-server' }),
     __metadata("design:paramtypes", [Object])
 ], HttpServerSubscription);
 exports.HttpServerSubscription = HttpServerSubscription;
+var HttpServerSubscription_1;
--- src/subscriptions/http-server-subscription.ts
+++ src/subscriptions/http-server-subscription.ts
@@ -7,29 +7,17 @@ import express from 'express';
 
 @Injectable({predicate: (subscriptionAttributes: any) => subscriptionAttributes.type === 'http-server'})
 export class HttpServerSubscription extends Subscription {
-    private app: any;
+    private static app: any = null;
+    private static server: any = null;
 
     private port: string;
     private endpoint: string;
-    private server: any;
     private response: any = {};
     private method: string;
 
     constructor(subscriptionAttributes: SubscriptionModel) {
         super(subscriptionAttributes);
-
-        this.app = express();
-        this.app.use((req: any, res: any, next: any) => {
-            req.setEncoding('utf8');
-            req.rawBody = '';
-            req.on('data', function(chunk: any) {
-                req.rawBody += chunk;
-            });
-            req.on('end', function() {
-                Logger.trace(`Http subscription read ${req.rawBody}`);
-                next();
-            });
-        });
+        this.initializeExpressApp();
 
         this.port = subscriptionAttributes.port;
         this.endpoint = subscriptionAttributes.endpoint;
@@ -39,15 +27,17 @@ export class HttpServerSubscription extends Subscription {
     }
 
     public receiveMessage(): Promise<string> {
-        return new Promise((resolve, reject) => {
-            this.app.all(this.endpoint, (request: any, response: any) => {
+        return new Promise((resolve) => {
+            HttpServerSubscription.app.all(this.endpoint, (request: any, response: any) => {
                 const payload = request.rawBody;
                 if (isNullOrUndefined(this.response.payload)) {
                     this.response.payload = payload;
                 }
+
                 for (const key in this.response.header) {
                     response.header(key, this.response.header[key]);
                 }
+
                 if (request.method != this.method) {
                     response.status(405).send(`Http server is expecting a ${this.method} call`);
                 } else {
@@ -60,20 +50,33 @@ export class HttpServerSubscription extends Subscription {
 
     public connect(): Promise<void> {
         return new Promise((resolve, reject) => {
-            this.server = this.app.listen(this.port, (err: any) => {
-                if (err) {
-                    reject(err);
-                }
+            if (HttpServerSubscription.server) {
                 resolve();
-            });
+            } else {
+                HttpServerSubscription.server = HttpServerSubscription.app.listen(this.port, (err: any) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve();
+                });
+            }
         });
     }
 
-    public unsubscribe(): void {
-        if (this.server) {
-            this.server.close();
+    private initializeExpressApp() {
+        if (!HttpServerSubscription.app) {
+            HttpServerSubscription.app = express();
+            HttpServerSubscription.app.use((req: any, res: any, next: any) => {
+                req.setEncoding('utf8');
+                req.rawBody = '';
+                req.on('data', function (chunk: any) {
+                    req.rawBody += chunk;
+                });
+                req.on('end', function () {
+                    Logger.trace(`Http subscription read ${req.rawBody}`);
+                    next();
+                });
+            });
         }
-        delete this.server;
     }
-
 }
