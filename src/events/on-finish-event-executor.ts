a273e717cbb6f3d6167291c82bcf0e0ed0875906
Guilherme Moraes <gmoraes@thoughtworks.com>
2018-09-03 20:19:03 -0300

    Adding file necessary to create onFinish concept. Always forgetting about that. :D

---
+++ src/events/on-finish-event-executor.ts
@@ -0,0 +1,24 @@
+import {Logger} from '../loggers/logger';
+import {EventExecutor} from './event-executor';
+import {TestModel} from '../models/outputs/test-model';
+import {Finishable} from './finishable';
+
+//TODO test it
+export class OnFinishEventExecutor extends EventExecutor {
+    private finishable: Finishable;
+
+    constructor(name: string, finishable: Finishable) {
+        super(finishable.onFinish);
+        this.finishable = finishable;
+        this.addArgument(name, this.finishable);
+    }
+
+    public trigger(): TestModel[] {
+        if (!this.finishable.onFinish) {
+            return [];
+        }
+        Logger.trace(`Executing onFinish`);
+        return this.execute();
+    }
+
+}
