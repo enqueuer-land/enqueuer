44ab452034b62be05d94b4db1bcf5b8d69f45e3b
Guilherme Moraes <gmoraes@thoughtworks.com>
2019-02-08 12:20:08 -0300

    Logging warning when fails to import injectable files

--- src/injectable-files-list.ts
+++ src/injectable-files-list.ts
@@ -1,156 +1,156 @@
 //Auto-Generated Code
 try {
-  require('./http-authentications/http-basic-authentication');
+   require('./http-authentications/http-basic-authentication');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './http-authentications/http-basic-authentication': ${err}`);
 }
 try {
-  require('./http-authentications/http-bearer-authentication');
+   require('./http-authentications/http-bearer-authentication');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './http-authentications/http-bearer-authentication': ${err}`);
 }
 try {
-  require('./http-authentications/http-digest-authentication');
+   require('./http-authentications/http-digest-authentication');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './http-authentications/http-digest-authentication': ${err}`);
 }
 try {
-  require('./http-authentications/http-no-authentication');
+   require('./http-authentications/http-no-authentication');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './http-authentications/http-no-authentication': ${err}`);
 }
 try {
-  require('./object-notations/csv');
+   require('./object-notations/csv');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './object-notations/csv': ${err}`);
 }
 try {
-  require('./object-notations/json');
+   require('./object-notations/json');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './object-notations/json': ${err}`);
 }
 try {
-  require('./object-notations/yaml');
+   require('./object-notations/yaml');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './object-notations/yaml': ${err}`);
 }
 try {
-  require('./outputs/formatters/console-formatter');
+   require('./outputs/formatters/console-formatter');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './outputs/formatters/console-formatter': ${err}`);
 }
 try {
-  require('./outputs/formatters/html-formatter');
+   require('./outputs/formatters/html-formatter');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './outputs/formatters/html-formatter': ${err}`);
 }
 try {
-  require('./outputs/formatters/json-formatter');
+   require('./outputs/formatters/json-formatter');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './outputs/formatters/json-formatter': ${err}`);
 }
 try {
-  require('./outputs/formatters/yml-formatter');
+   require('./outputs/formatters/yml-formatter');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './outputs/formatters/yml-formatter': ${err}`);
 }
 try {
-  require('./publishers/custom-publisher');
+   require('./publishers/custom-publisher');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './publishers/custom-publisher': ${err}`);
 }
 try {
-  require('./publishers/file-publisher');
+   require('./publishers/file-publisher');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './publishers/file-publisher': ${err}`);
 }
 try {
-  require('./publishers/http-publisher');
+   require('./publishers/http-publisher');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './publishers/http-publisher': ${err}`);
 }
 try {
-  require('./publishers/null-publisher');
+   require('./publishers/null-publisher');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './publishers/null-publisher': ${err}`);
 }
 try {
-  require('./publishers/standard-output-publisher');
+   require('./publishers/standard-output-publisher');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './publishers/standard-output-publisher': ${err}`);
 }
 try {
-  require('./publishers/stream-publisher');
+   require('./publishers/stream-publisher');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './publishers/stream-publisher': ${err}`);
 }
 try {
-  require('./publishers/udp-publisher');
+   require('./publishers/udp-publisher');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './publishers/udp-publisher': ${err}`);
 }
 try {
-  require('./run-modes/daemon-run-executor');
+   require('./run-modes/daemon-run-executor');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './run-modes/daemon-run-executor': ${err}`);
 }
 try {
-  require('./run-modes/daemon-run-input/http-daemon-input');
+   require('./run-modes/daemon-run-input/http-daemon-input');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './run-modes/daemon-run-input/http-daemon-input': ${err}`);
 }
 try {
-  require('./run-modes/daemon-run-input/null-daemon-input');
+   require('./run-modes/daemon-run-input/null-daemon-input');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './run-modes/daemon-run-input/null-daemon-input': ${err}`);
 }
 try {
-  require('./run-modes/daemon-run-input/tcp-daemon-input');
+   require('./run-modes/daemon-run-input/tcp-daemon-input');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './run-modes/daemon-run-input/tcp-daemon-input': ${err}`);
 }
 try {
-  require('./run-modes/daemon-run-input/uds-daemon-input');
+   require('./run-modes/daemon-run-input/uds-daemon-input');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './run-modes/daemon-run-input/uds-daemon-input': ${err}`);
 }
 try {
-  require('./run-modes/single-run-executor');
+   require('./run-modes/single-run-executor');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './run-modes/single-run-executor': ${err}`);
 }
 try {
-  require('./subscriptions/custom-subscription');
+   require('./subscriptions/custom-subscription');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './subscriptions/custom-subscription': ${err}`);
 }
 try {
-  require('./subscriptions/filename-watcher-subscription');
+   require('./subscriptions/filename-watcher-subscription');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './subscriptions/filename-watcher-subscription': ${err}`);
 }
 try {
-  require('./subscriptions/http-subscription');
+   require('./subscriptions/http-subscription');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './subscriptions/http-subscription': ${err}`);
 }
 try {
-  require('./subscriptions/null-subscription');
+   require('./subscriptions/null-subscription');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './subscriptions/null-subscription': ${err}`);
 }
 try {
-  require('./subscriptions/standard-input-subscription');
+   require('./subscriptions/standard-input-subscription');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './subscriptions/standard-input-subscription': ${err}`);
 }
 try {
-  require('./subscriptions/stream-subscription');
+   require('./subscriptions/stream-subscription');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './subscriptions/stream-subscription': ${err}`);
 }
 try {
-  require('./subscriptions/udp-subscription');
+   require('./subscriptions/udp-subscription');
 } catch (err) {
-  /* do nothing*/
+    console.warn(`Error loading './subscriptions/udp-subscription': ${err}`);
 }
